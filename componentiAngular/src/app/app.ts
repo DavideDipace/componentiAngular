2d9eb8d0cba83eb6a14caf07896a670ec2d58983
DavideDipace <113579525+DavideDipace@users.noreply.github.com>
2025-10-21 07:50:04 +0000

    esercizi finiti

--- componentiAngular/src/app/app.ts
+++ componentiAngular/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { Esercizio1 } from './esercizio1/esercizio1';
+import { Esercizio2 } from './esercizio2/esercizio2';
+import { Esercizio3 } from './esercizio3/esercizio3';
 
 @Component({
   selector: 'app-root',
-  imports: [Esercizio1],
+  imports: [Esercizio1, Esercizio2, Esercizio3],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
